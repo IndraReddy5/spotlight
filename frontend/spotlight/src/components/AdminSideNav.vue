4f0e1b93401b7bb8d1722d621b6787c7267be588
IndraReddy5 <45683173+IndraReddy5@users.noreply.github.com>
2023-11-07 12:14:21 +0530

    added d3 and admin dashboard temp ui

---
+++ frontend/spotlight/src/components/AdminSideNav.vue
@@ -0,0 +1,147 @@
+<template>
+    <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-dark sidebar collapse" style="">
+      <div class="position-sticky pt-3">
+        <h6 class="d-flex justify-content-between align-items-center ps-1 mt-4 mb-1">
+          <a class="nav-link active" aria-current="page" href="#">Creator Requests</a>
+        </h6>
+        <ul class="nav flex-column mb-2">
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              user 1
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              user 2
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              user 3
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              user 4
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              No new Requests
+            </a>
+          </li>
+        </ul>
+        <h6 class="d-flex justify-content-between align-items-center ps-1 mt-4 mb-1">
+          <a class="nav-link active" aria-current="page" href="#">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" stroke="currentColor"
+              stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"
+              aria-hidden="true">
+              <path
+                d="M20 2H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-2 5h-3v5.5c0 1.38-1.12 2.5-2.5 2.5S10 13.88 10 12.5s1.12-2.5 2.5-2.5c.57 0 1.08.19 1.5.51V5h4v2zM4 6H2v14c0 1.1.9 2 2 2h14v-2H4V6z" />
+            </svg>
+            Genre Requests
+            <a class="link-secondary" href="#" aria-label="Add a new report">
+              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none"
+                stroke="var(--bp-khaki)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                class="feather feather-plus-circle" aria-hidden="true">
+                <!-- <circle cx="12" cy="12" r="10"></circle> -->
+                <line x1="12" y1="8" x2="12" y2="16"></line>
+                <line x1="8" y1="12" x2="16" y2="12"></line>
+              </svg>
+            </a>
+          </a>
+        </h6>
+        <ul class="nav flex-column mb-3">
+          <li class="nav-item">
+            <h6 class="d-flex justify-content-between align-items-center mt-2 mb-1">
+              <a class="nav-link" href="#">Genre 1</a>
+            </h6>
+          </li>
+          <li class="nav-item">
+            <h6 class="d-flex justify-content-between align-items-center mt-2 mb-1">
+              <a class="nav-link" href="#">Genre 2</a>
+            </h6>
+          </li>
+          <li class="nav-item">
+            <h6 class="d-flex justify-content-between align-items-center mt-2 mb-1">
+              <a class="nav-link" href="#">Genre 3</a>
+            </h6>
+          </li>
+          <li class="nav-item">
+            <h6 class="d-flex justify-content-between align-items-center mt-2 mb-1">
+              <a class="nav-link" href="#">Genre 4</a>
+            </h6>
+          </li>
+          <li class="nav-item">
+            <h6 class="d-flex justify-content-between align-items-center mt-2 mb-1">
+              <a class="nav-link" href="#">No new Requests</a>
+            </h6>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </template>
+  <script>
+  export default {
+    name: 'AdminSideNav',
+  }
+  </script>
+  <style>
+  .sidebar {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    z-index: 100;
+    padding: 48px 0 0;
+    box-shadow: inset -1px 0 0 rgba(0, 0, 0, .1);
+    overflow-x: hidden;
+    overflow-y: auto;
+    /* Hide scrollbar for IE, Edge and Firefox */
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+  }
+  
+  /* Hide scrollbar for Chrome, Safari and Opera */
+  .sidebar::-webkit-scrollbar {
+    display: none;
+  }
+  
+  @media (max-width: 767.98px) {
+    .sidebar {
+      top: 5rem;
+    }
+  }
+  
+  .sidebar-sticky {
+    position: relative;
+    top: 0;
+    height: calc(100vh - 48px);
+    padding-top: .5rem;
+    overflow-x: hidden;
+    overflow-y: hidden;
+  }
+  
+  .sidebar .nav-link {
+    font-weight: 500;
+    color: #fff;
+  }
+  
+  .sidebar .nav-link .feather {
+    margin-right: 4px;
+    color: var(--bp-khaki);
+  }
+  
+  .sidebar .nav-link.active {
+    color: var(--bp-khaki);
+  }
+  
+  .sidebar .nav-link:hover .feather,
+  .sidebar .nav-link.active .feather {
+    color: var(--bp-khaki);
+  }
+  
+  .sidebar a:hover {
+    color: var(--bp-khaki);
+  }
+  </style>
--- frontend/spotlight/src/views/AdminDashboard.vue
+++ frontend/spotlight/src/views/AdminDashboard.vue
@@ -3,31 +3,241 @@
         <TopNav></TopNav>
         <div class="container-fluid">
             <div class="row">
-                <SideNav></SideNav>
+                <AdminSideNav></AdminSideNav>
                 <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-                    <canvas class="my-4 w-100" id="myChart" width="1351" height="570"
-                        style="display: block; box-sizing: border-box; height: 380px; width: 900px;"></canvas>
+                    <h2 class="pt-5">Admin Dashboard</h2>
+                    <div class="container pt-5">
+                        <div class="row g-0 text-center">
+                            <div class="col menu_board p-2 m-2 rounded">
+                                <div>
+                                    <h5>Pending Album Flag Req</h5>
+                                </div>
+                                <div>25</div>
+                            </div>
+                            <div class="col menu_board p-2 m-2 rounded">
+                                <div>
+                                    <h5>Pending Song Flag Req</h5>
+                                </div>
+                                <div>10</div>
+                            </div>
+                            <div class="col menu_board p-2 m-2 rounded">
+                                <div>
+                                    <h5>Current Logged in users Number</h5>
+                                </div>
+                                <div>50</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="container pt-5">
+                        <div class="row g-0 text-center">
+                            <div class="col menu_board p-2 m-2 rounded">
+                                <div>
+                                    <h5>Total Users</h5>
+                                </div>
+                                <div>200</div>
+                            </div>
+                            <div class="col menu_board p-2 m-2 rounded">
+                                <div>
+                                    <h5>Melophiles</h5>
+                                </div>
+                                <div>125</div>
+                            </div>
+                            <div class="col menu_board p-2 m-2 rounded">
+                                <div>
+                                    <h5>Patrons</h5>
+                                </div>
+                                <div>50</div>
+                            </div>
+                            <div class="col menu_board p-2 m-2 rounded">
+                                <div>
+                                    <h5>Creators</h5>
+                                </div>
+                                <div>25</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="container pt-5 position-relative">
+                        <div class="row g-0 justify-content-between">
+                            <div class="col menu_board p-2 m-2 rounded text-center">
+                                <svg id="canvasPie" class="w-100 p-2"></svg>
+                            </div>
+                            <div class="col menu_board p-2 m-2 rounded text-center">
+                                <svg id="canvas1" class="w-100 p-2"></svg>
+                            </div>
+                        </div>
+                    </div>
                 </main>
             </div>
         </div>
     </div>
 </template>
 <script>
-import SideNav from '@/components/SideNav.vue';
+import AdminSideNav from '@/components/AdminSideNav.vue';
 import TopNav from '@/components/TopNav.vue';
-import AlbumComponent from '@/components/AlbumComponent.vue';
-import SongComponent from '@/components/SongComponent.vue'
-import CreatorStats from '@/components/CreatorStats.vue';
+import * as d3 from "d3";
 
 export default {
     name: 'AdminDashboard',
     components: {
-        SideNav,
+        AdminSideNav,
         TopNav,
-        AlbumComponent,
-        SongComponent,
-        CreatorStats,
+    },
+    mounted() {
+        const width = 300;
+        const height = 300;
+        const marginTop = 20;
+        const marginRight = 0;
+        const marginBottom = 30;
+        const marginLeft = 40;
+        const bar_data = [{ "letter": "1-2", "frequency": 2 },
+        { "letter": "2-3", "frequency": 5 },
+        { "letter": "3-3.5", "frequency": 10 },
+        { "letter": "3.5-4", "frequency": 25 },
+        { "letter": "4-4.5", "frequency": 50 },
+        { "letter": "4.5-5", "frequency": 90 },
+        ];
+
+        const x = d3.scaleBand()
+            .domain(d3.sort(bar_data, d => -d.frequency).map(d => d.letter))
+            .range([marginLeft, width - marginRight])
+            .padding(0.1);
+
+        const xAxis = d3.axisBottom(x).tickSizeOuter(0);
+
+        const y = d3.scaleLinear()
+            .domain([0, d3.max(bar_data, d => d.frequency)]).nice()
+            .range([height - marginBottom, marginTop]);
+
+        const svg = d3.select("#canvas1")
+            .attr("viewBox", [0, 0, width, height])
+            .attr("width", width)
+            .attr("height", height)
+            .attr("style", "max-width: 100%; height: auto;")
+            .call(zoom);
+
+        const g = svg.append("g");
+
+        g.append("g")
+            .attr("class", "bars")
+            .attr("fill", "steelblue")
+            .selectAll("rect")
+            .data(bar_data)
+            .join("rect")
+            .attr("x", d => x(d.letter))
+            .attr("y", d => y(d.frequency))
+            .attr("height", d => y(0) - y(d.frequency))
+            .attr("width", x.bandwidth());
+
+        g.append("g")
+            .attr("class", "x-axis")
+            .attr("transform", `translate(0,${height - marginBottom})`)
+            .call(xAxis);
+
+        g.append("g")
+            .attr("class", "y-axis")
+            .attr("transform", `translate(${marginLeft},0)`)
+            .call(d3.axisLeft(y))
+            .call(g => g.select(".domain").remove());
+
+        svg.append("text")
+            .attr("transform", `translate(180, 40)`)
+            .style("font", "20px sans-serif")
+            .style("fill", "#fff")
+            .text("Total Songs");
+
+        svg.append("text")
+            .attr("transform", `translate(220, 70)`)
+            .style("font", "20px sans-serif")
+            .style("fill", "#fff")
+            .text("45");
+
+        function zoom(svg) {
+            const extent = [[marginLeft, marginTop], [width - marginRight, height - marginTop]];
+
+            svg.call(d3.zoom()
+                .scaleExtent([1, 6])
+                .translateExtent(extent)
+                .extent(extent)
+                .on("zoom", zoomed));
+
+            function zoomed(event) {
+                x.range([marginLeft, width - marginRight].map(d => event.transform.applyX(d)));
+                svg.selectAll(".bars rect").attr("x", d => x(d.letter)).attr("width", x.bandwidth());
+                svg.selectAll(".x-axis").call(xAxis);
+            }
+        }
+        const data = [{"name":"Classic","value":30},
+        {"name":"Blues","value":15},
+        {"name":"Metal","value":50},
+        {"name":"Lofi","value":40},
+        {"name":"Indie","value":25}
+    ];
+        const color = d3.scaleOrdinal()
+            .domain(data.map(d => d.name))
+            .range(d3.quantize(t => d3.interpolateSpectral(t * 0.8 + 0.1), data.length).reverse())
+        const pie = d3.pie()
+            .sort(null)
+            .value(d => d.value);
+
+        const arc = d3.arc()
+            .innerRadius(0)
+            .outerRadius(Math.min(width, height) / 2 - 1);
+
+        const labelRadius = arc.outerRadius()() * 0.8;
+
+        // A separate arc generator for labels.
+        const arcLabel = d3.arc()
+            .innerRadius(labelRadius)
+            .outerRadius(labelRadius);
+
+        const arcs = pie(data);
+
+        // Create the SVG container.
+        const pie_svg = d3.select("#canvasPie")
+            .attr("width", width)
+            .attr("height", height)
+            .attr("viewBox", [-width / 2, -height / 2, width, height])
+            .attr("style", "max-width: 100%; height: auto; font: 10px sans-serif;");
+
+        pie_svg.append("g")
+            .attr("stroke", "white")
+            .selectAll()
+            .data(arcs)
+            .join("path")
+            .attr("fill", d => color(d.data.name))
+            .attr("d", arc)
+            .append("title")
+            .text(d => `${d.data.name}: ${d.data.value.toLocaleString("en-US")}`);
+
+        // Create a new arc generator to place a label close to the edge.
+        // The label shows the value if there is enough room.
+        pie_svg.append("g")
+            .attr("text-anchor", "middle")
+            .selectAll()
+            .data(arcs)
+            .join("text")
+            .attr("transform", d => `translate(${arcLabel.centroid(d)})`)
+            .call(text => text.append("tspan")
+                .attr("y", "-0.4em")
+                .attr("font-weight", "bold")
+                .text(d => d.data.name))
+            .call(text => text.filter(d => (d.endAngle - d.startAngle) > 0.25).append("tspan")
+                .attr("x", 0)
+                .attr("y", "0.7em")
+                .attr("fill-opacity", 0.7)
+                .text(d => d.data.value.toLocaleString("en-US")));
+
     }
 }
 </script>
-<style></style>
+<style>
+/* This controls the Chart Title */
+text .title {
+    font-size: 16px;
+}
+
+/* This controls the axis text size */
+.axis text {
+    font-size: 8px;
+}
+</style>
